e4d3d1794d8fe3a0520767f0ed24e389a3c302e2
Wilfred Marete <marete015@gmail.com>
2023-03-27 01:21:07 +0300

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-28io.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
