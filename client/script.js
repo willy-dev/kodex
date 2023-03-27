8a1d4fd82d47d54d1970110b3ab5b6cb3bb972a3
Wilfred Marete <marete015@gmail.com>
2023-03-27 22:55:41 +0300

    connect to deployed server link

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://kodexserver.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
